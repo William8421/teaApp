917ea914e61ef4863ea3b542b93c79ef533ca7f5
william <william.mallak1@gmail.com>
2022-06-15 09:33:42 +0200

    need more editing

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from 'dotenv';
+import {routerMain} from './routes/main.js'
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.SERVER_PORT || 5000;
+const home = '/';
+app.use(home, routerMain)
 
 
 
